90c2baf0f2c9b4311269ed01eeba42f25ab4cbcc
lemonade4813 <lemonade4813@gmail.com>
2022-09-08 14:01:15 +0900

    commit 220908

--- es6-dev-environment/index.js
+++ es6-dev-environment/index.js
@@ -7,15 +7,19 @@ const config = require('./config/key')
 
 const {User} = require("./User")
 
+
+
 //appication/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:false}))
 
 //applicaton/json
 app.use(bodyParser.json())
-app.use(cookieParser());
+app.use(cookieParser())
+app.use(express.json())
 
 const mongoose = require('mongoose')
 const { JsonWebTokenError } = require('jsonwebtoken')
+
 mongoose.connect(config.mongoURI).then(()=> console.log('MongoDB connected'))
   .catch(err=>console.log(err)) 
 
@@ -49,16 +53,22 @@ app.post('/register', (req, res) => {
 app.post('/login', (req, res)=>{
 
   //요청된 이메일을 데이터베이스에서 있는지 찾는다.
-  User.findOne({email: req.body.email}, (err, user) => {
+  User.find({email : req.body.email}, (err, user) => {
       if(!user){
           return res.json({
               loginSuccess : false,
               message:"제공된 이메일에 해당하는 유저가 없습니다."
           })
-
       }
+     
+     /* User.findOne({email:req.body.email}).exec((err, detail)=>{
+        if(err) return res.status(400).send(err);
+  
+        return res.status(200).json(detail)
+  
+    }) */
   //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
-  user.comparePassword(req.body.password,(err, isMatch)=>{
+ /* user.comparePassword(req.body.password,(err, isMatch)=>{
       if(!isMatch)
           return res.json({loginSuccess : false, message : "비밀번호가 틀렸습니다." })
 
@@ -72,10 +82,12 @@ app.post('/login', (req, res)=>{
           .status(200)
           .json({loginSuccess:true, userId: user._id})
          })
-      })
-  })
-
+      }) */
+  
+   
+    })
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
